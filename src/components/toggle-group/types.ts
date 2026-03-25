42eaa66f6af0634b90d5e4b1f75ef59d9eaa2e41
Löwe <henning.lowe@aize.io>
2026-03-25 20:13:56 +0100

    feat(toggle-group): add types

---
+++ src/components/toggle-group/types.ts
@@ -0,0 +1,15 @@
+/** Detail for `grund-value-change` event on the root element. */
+export interface ToggleGroupValueChangeDetail {
+  value: string[];
+}
+
+/**
+ * Snapshot of host properties passed to ToggleGroupController.syncFromHost().
+ * @internal
+ */
+export interface ToggleGroupHostSnapshot {
+  value: string[] | undefined;
+  defaultValue: string[];
+  multiple: boolean;
+  disabled: boolean;
+}
