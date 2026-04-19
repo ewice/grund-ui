828d26cbcdbb7b71bfd45e7011f5573ab90d44a6
ewice <henning.loewe@hotmail.de>
2026-04-19 21:55:47 +0200

    feat(avatar): add barrel export and package entrypoint

---
+++ src/components/avatar/index.ts
@@ -0,0 +1,4 @@
+export { GrundAvatar } from './avatar';
+export { GrundAvatarImage } from './avatar-image';
+export { GrundAvatarFallback } from './avatar-fallback';
+export type { AvatarStatus, AvatarStatusChangeDetail } from './types';
