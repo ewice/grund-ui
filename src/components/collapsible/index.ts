221cf2abb9e483da73b27e327bc1d2bed0d7925a
ewice <henning.loewe@hotmail.de>
2026-04-30 09:40:26 +0200

    feat(collapsible): add barrel exports and package entry point

---
+++ src/components/collapsible/index.ts
@@ -0,0 +1,5 @@
+export { GrundCollapsible } from './collapsible';
+export { GrundCollapsibleTrigger } from './collapsible-trigger';
+export { GrundCollapsiblePanel } from './collapsible-panel';
+
+export type { CollapsibleOpenChangeDetail, CollapsibleOpenChangeReason } from './types';
